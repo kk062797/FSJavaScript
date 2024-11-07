a8b4e752e0ceeeb593ed70f1376177a762a3791d
kk062797 <kanishk423@gmail.com>
2024-11-07 10:14:54 +0530

    day2

--- day2.js
+++ day2.js
@@ -1,1 +1,15 @@
-console.log("Kanishk");
+firstName = "Kanishk";
+
+thirdName = null;
+secondName = undefined;
+
+console.log(typeof firstName);
+
+firstName = 2;
+console.log(typeof firstName);
+
+firstName = true;
+console.log(typeof firstName);
+
+console.log(typeof secondName);
+console.log(typeof thirdName);
