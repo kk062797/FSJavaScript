4b44e2459c1d87869db0d827894545332401d25d
kk062797 <kanishk423@gmail.com>
2024-11-21 13:35:23 +0530

    day10

---
+++ day10.js
@@ -0,0 +1,45 @@
+// slice
+/*
+const fName = `Kanishk`;
+console.log(fName.slice(-5,-3));
+
+//Split
+const sentence = `My name is Kanishk. I am Software Test Engineer.`;
+console.log(sentence.split(" "));
+*/
+
+//Arrays
+const arr = [0,1,2,3,4,5,6];
+/*
+console.log(arr[5]);
+console.log(arr[-2]);
+//using at()
+console.log(arr.at(5));
+console.log(arr.at(-2));
+*/
+/*
+//slice
+const arr1 = arr.slice(4,6);
+console.log(`Slice Array: ${arr1}`);
+console.log(`Original Array: ${arr}`);
+
+//splice
+const arr2 = arr.splice(4);
+console.log(`Original Array: ${arr}`);
+console.log(`Splice Array: ${arr2}`);
+*/
+const arr3 = arr.reverse();
+console.log(`Original Array: ${arr}`);
+console.log(`Reverse Array: ${arr3}`);
+
+
+
+
+
+
+
+
+
+
+
+
