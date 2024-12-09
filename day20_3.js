af5b5c788ffd655290f0311b3e9062bbd9c23455
kk062797 <kanishk423@gmail.com>
2024-12-09 19:14:35 +0530

    day21

--- day20_3.js
+++
@@ -1,3 +0,0 @@
-import {emp} from "./day20_2.js"
-
-console.log(emp);
---
+++ day21.js
@@ -0,0 +1,4 @@
+console.log("Start");
+setTimeout(()=>{console.log("Executed1")},5000);
+setTimeout(()=>{console.log("Executed2")},1000);
+console.log("End");
