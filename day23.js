c6970cbfe6c49cb553fb84347f9c61a952f72b4f
kk062797 <kanishk423@gmail.com>
2024-12-12 13:13:40 +0530

    day23

--- day23.js
+++ day23.js
@@ -1,7 +1,22 @@
+// Handle promises using then
+
 const url = "https://reqres.in/api/users/2"
+/*
 console.log("Start");
 fetch(url)
 //.then(res => console.log(res))
 .then(res => res.json())
 .then(data => console.log(data))
 .finally(() => console.log("End"));
+*/
+
+// Handle promises using await
+
+console.log("Start");
+async function getOutput() {
+    const res = await fetch(url)
+    const data = await res.json();
+    console.log(data);
+}
+getOutput();
+console.log("Process Completed")
